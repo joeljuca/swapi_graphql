4add9ac463b631835cb0c5cfaba188484a8e4635
Joel Wallis Jucá <joelwallis@gmail.com>
2019-02-21 12:11:59 -0300

    Adds poison as dependency

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule SwapiGraphql.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:poison, "~> 4.0"}
     ]
   end
 end
