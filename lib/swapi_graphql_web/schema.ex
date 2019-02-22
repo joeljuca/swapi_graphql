bfad40bbc23a248e1cf3c577ccb809bc5c18539b
Joel Wallis Jucá <joelwallis@gmail.com>
2019-02-22 11:51:06 -0300

    Adds resolvers with mocked data for all object types

--- lib/swapi_graphql_web/schema.ex
+++ lib/swapi_graphql_web/schema.ex
@@ -2,12 +2,24 @@ defmodule SwapiGraphqlWeb.Schema do
   use Absinthe.Schema
 
   query do
-    field :films,     list_of(:film)
-    field :people,    list_of(:person)
-    field :planets,   list_of(:planet)
-    field :species,   list_of(:specie)
-    field :starships, list_of(:starship)
-    field :vehicles,  list_of(:vehicle)
+    field :films,     list_of(:film) do
+      resolve fn _, _, _ -> {:ok, [ %{ :id => 1, :title => "Film 1" } ]} end
+    end
+    field :people,    list_of(:person) do
+      resolve fn _, _, _ -> {:ok, [ %{ :id => 1, :name => "Person 1" } ]} end
+    end
+    field :planets,   list_of(:planet) do
+      resolve fn _, _, _ -> {:ok, [ %{ :id => 1, :name => "Planet 1" } ]} end
+    end
+    field :species,   list_of(:specie) do
+      resolve fn _, _, _ -> {:ok, [ %{ :id => 1, :name => "Specie 1" } ]} end
+    end
+    field :starships, list_of(:starship) do
+      resolve fn _, _, _ -> {:ok, [ %{ :id => 1, :name => "Starship 1" } ]} end
+    end
+    field :vehicles,  list_of(:vehicle) do
+      resolve fn _, _, _ -> {:ok, [ %{ :id => 1, :name => "Vehicle 1" } ]} end
+    end
   end
 
   object :film do
