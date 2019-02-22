f85ae8e0799739e6d311f089ff93784a2a384733
Joel Wallis Jucá <joelwallis@gmail.com>
2019-02-22 11:16:22 -0300

    Revert "Adds poison as dependency"
    This reverts commit 4add9ac463b631835cb0c5cfaba188484a8e4635.

--- mix.exs
+++ mix.exs
@@ -39,8 +39,7 @@ defmodule SwapiGraphql.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:poison, "~> 4.0"}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 end
