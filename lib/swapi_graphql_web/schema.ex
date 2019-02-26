814590afd0fc17d10732a748fc09cc985dbc934a
Joel Wallis Jucá <joelwallis@gmail.com>
2019-02-26 15:38:35 -0300

    Adds a string argument q to all GraphQL fields

--- lib/swapi_graphql_web/schema.ex
+++ lib/swapi_graphql_web/schema.ex
@@ -9,21 +9,32 @@ defmodule SwapiGraphqlWeb.Schema do
 
   query do
     field :films,     list_of(:film) do
+      arg :q, :string
       resolve(&FilmResolver.resolve/3)
     end
+
     field :people,    list_of(:person) do
+      arg :q, :string
       resolve(&PersonResolver.resolve/3)
     end
+
     field :planets,   list_of(:planet) do
+      arg :q, :string
       resolve(&PlanetResolver.resolve/3)
     end
+
     field :species,   list_of(:specie) do
+      arg :q, :string
       resolve(&SpecieResolver.resolve/3)
     end
+
     field :starships, list_of(:starship) do
+      arg :q, :string
       resolve(&StarshipResolver.resolve/3)
     end
+
     field :vehicles,  list_of(:vehicle) do
+      arg :q, :string
       resolve(&VehicleResolver.resolve/3)
     end
   end
